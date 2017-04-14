a05998b12bf74af045470748c62f335bab223ffd
Nasa Nguyen <nasa8x@gmail.com>
2017-04-14 08:20:33 +0700

    fix file ext handler

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 'use strict';
 var _ = require('mix-utils');
-var _ext = (/\.(js|css|xml|less|doc|txt|docx|ico|zip|rar|mp3|exe|avi|mp4|mpg|mpeg|psd|ai|xsl|m4a|wmv|rss|ppt|flv|swf|dat|dmg|iso|m4v|torrent|gif|jpg|jpeg|tiff|png)$/i);
+var _ext = /\.(js|css|xml|less|doc|txt|docx|ico|zip|rar|mp3|exe|avi|mp4|mpg|mpeg|psd|ai|xsl|m4a|wmv|rss|ppt|flv|swf|dat|dmg|iso|m4v|torrent|gif|jpg|jpeg|tiff|png)$/i;
 
 /**
  * register the plug-in with the Hapi framework
@@ -10,10 +10,10 @@ var _ext = (/\.(js|css|xml|less|doc|txt|docx|ico|zip|rar|mp3|exe|avi|mp4|mpg|mpe
  * @param  {Function} next
  */
 var register = function (server, options, next) {
-    var conf = Object.assign({}, { enabled: true, view: 'index.html', data:{} }, options || {});
-    server.ext('onRequest', function (request, reply) {        
+    var conf = Object.assign({}, { enabled: true, view: 'index.html', data: {} }, options || {});
+    server.ext('onRequest', function (request, reply) {
         conf = Object.assign({}, conf, request.route.settings.plugins.seo || {});
-        if (conf.enabled && request.method === 'get' && _.isCrawl(request.headers['user-agent']) && !_ext.test(request.url))
+        if ((conf.enabled && request.method === 'get' && _.isCrawl(request.headers['user-agent'])) || _ext.test(request.url.pathname))        
             reply.continue();
         else
             reply.view(conf.view, conf.data);
